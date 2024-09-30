90e15906aed66d1be6593f02bf3ffd4aaf5bed62
Cedrick Campoto <cedrick.campoto@yahoo.com>
2024-09-30 09:25:47 +0800

    add lifecycle

--- src/consts/component-lifecycle.ts
+++ src/consts/component-lifecycle.ts
@@ -1,5 +1,12 @@
+export const BEFORE_RENDERED = "before_rendered";
 export const RENDERED = "rendered";
 export const DESTROY = "destroy";
 export const CHANGE = "change";
 export const ADOPTED = "adopted";
-export const COMPONENT_LIFECYCLE = { RENDERED, DESTROY, CHANGE, ADOPTED };
+export const COMPONENT_LIFECYCLE = {
+  BEFORE_RENDERED,
+  RENDERED,
+  DESTROY,
+  CHANGE,
+  ADOPTED,
+};
